069050ae0d0b7d963ac401f1e233150ccb8ad4da
Ovi Isai <ovidiu.isai@sixt.com>
2015-09-24 09:29:02 +0200

    adding some better idempotent flow

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,24 @@
 
 class lombok {
 	
-	$tar_filename = 'lombok.jar'
+    $tar_filename = 'lombok.jar'
 
-	exec {"wget_lombok":
-            command => "/usr/bin/wget --progress=bar https://projectlombok.org/downloads/${tar_filename}",
-            unless  => "/usr/bin/test -f /tmp/${tar_filename}",
-            cwd     => "/tmp"
-            #timeout => 0
+    exec {"wget_lombok":
+        command => "/usr/bin/wget --progress=bar https://projectlombok.org/downloads/${tar_filename}",
+        unless  => "/usr/bin/test -f /tmp/${tar_filename}",
+        cwd     => "/tmp"
     }
 
-	exec {"copy_lombok_jar":
-            command => "/bin/cp /tmp/${tar_filename} /opt/sts-bundle/sts-home",
-            unless  => "/usr/bin/test -f /opt/sts-bundle/sts-home/${tar_filename}",
-            cwd     => "/opt",
-            require => Exec["wget_lombok"],	
+    exec {"copy_lombok_jar":
+        command => "/bin/cp /tmp/${tar_filename} /opt/sts-bundle/sts-home",
+        unless  => "/usr/bin/test -f /opt/sts-bundle/sts-home/${tar_filename}",
+        cwd     => "/opt",
+#        require => Exec["wget_lombok"],	
     }
 
-	file {"/tmp/$tar_filename":
-		ensure => absent,
-		require => Exec["copy_lombok_jar"]
-	}
+#    file {"/tmp/$tar_filename":
+#    	ensure => absent,
+#	require => Exec["copy_lombok_jar"]
+#   }
 
 }
